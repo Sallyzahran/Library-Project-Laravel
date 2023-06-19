dee97b4e14c0329a7326f8bc731544bdc3e4f68b
Sallyzahran <sallyzahran.sz@gmail.com>
2023-06-19 21:28:22 +0300

    edit in bookController

--- app/Http/Controllers/Api/bookController.php
+++ app/Http/Controllers/Api/bookController.php
@@ -1,7 +1,7 @@
 <?php
+namespace App\Http\Controllers\Api;
 
-namespace App\Http\Controllers;
-
+use App\Http\Controllers\Controller;
 use App\Http\Services\Media;
 
 use App\Http\Requests\StoreBookRequest;
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\bookController;
+use App\Http\Controllers\Api\bookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
