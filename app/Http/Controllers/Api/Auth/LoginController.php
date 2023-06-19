8e7e7c0ac0aa5bb6fa627dc6dde8542854c3fae0
monasrour <mona.omar.srour@gmail.com>
2023-06-19 03:24:11 +0300

    make login Controller and validated data

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        // Validate the request data
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+   
+    }
+}
